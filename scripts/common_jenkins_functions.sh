07a472225a99e2cac3d2adf62f26c4b5c0e15bb3
Brian Manning <brian@xaoc.org>
2013-03-17 00:22:13 -0700

    added getopt function that calls system getopt

--- scripts/common_jenkins_functions.sh
+++ scripts/common_jenkins_functions.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # other possible choices here are /bin/bash or maybe /bin/ksh
 
 # Library of shell functions used with Jenkins job steps
@@ -30,6 +30,43 @@ check_exit_status () {
     return $EXIT_STATUS
 } # check_exit_status
 
+run_getopt() {
+    local GETOPT_SHORT="$1"
+    local GETOPT_LONG="$2"
+
+    # these two paths cover a majority of my test machines
+    for GETOPT_CHECK in "/opt/local/bin/getopt" "/usr/bin/getopt";
+    do
+        if [ -x "${GETOPT_CHECK}" ]; then
+            GETOPT_BIN=$GETOPT_CHECK
+            break
+        fi
+    done
+
+    # did we find an actual binary out of the list above?
+    if [ -z "${GETOPT_BIN}" ]; then
+        echo "ERROR: getopt binary not found; exiting...."
+        exit 1
+    fi
+
+    # Use short options if we're using Darwin's getopt
+    if [ $OSDETECT = "Darwin" -a $GETOPT_BIN != "/opt/local/bin/getopt" ]; then
+        GETOPT_TEMP=$(${GETOPT_BIN} ${GETOPT_SHORT} $*)
+    else
+    # Use short and long options with GNU's getopt
+        GETOPT_TEMP=$(${GETOPT_BIN} -o ${GETOPT_SHORT} \
+            --long ${GETOPT_LONG} \
+            -n "${SCRIPTNAME}" -- "$@")
+    fi
+
+    # if getopts exited with an error code, then exit the script
+    #if [ $? -ne 0 -o $# -eq 0 ] ; then
+    if [ $? != 0 ] ; then
+        echo "Run '${SCRIPTNAME} --help' to see script options"
+        exit 1
+    fi
+}
+
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation; version 2 dated June, 1991.
@@ -41,7 +78,8 @@ check_exit_status () {
 #
 #   You should have received a copy of the GNU General Public License
 #   along with this program;  if not, write to the Free Software
-#   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111, USA.
+#   Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 
 # vi: set filetype=sh shiftwidth=4 tabstop=4
 # end of line

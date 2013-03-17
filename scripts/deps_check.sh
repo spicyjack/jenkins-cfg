b5420e540147fa627f861d78cfb1599ae5ebb4c9
Brian Manning <brian@xaoc.org>
2013-03-17 00:18:55 -0700

    updated FSF's address

--- scripts/deps_check.sh
+++ scripts/deps_check.sh
@@ -143,7 +143,8 @@ exit $EXIT_STATUS
 #
 #   You should have received a copy of the GNU General Public License
 #   along with this program;  if not, write to the Free Software
-#   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111, USA.
+#   Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 
 # vi: set filetype=sh shiftwidth=4 tabstop=4
 # end of line

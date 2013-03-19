378ecf470ad4deabe3091191a87006e4e480dbf4
Brian Manning <brian@xaoc.org>
2013-03-19 12:07:08 -0700

    added simple delete script

---
+++ scripts/delete_output_dirs.sh
@@ -0,0 +1,121 @@
+#!/bin/sh
+
+# Delete output directories
+
+# Copyright (c)2013 by Brian Manning (brian at xaoc dot org)
+# License terms are listed at the bottom of this file
+#
+# Impotant URLs:
+# Clone:    https://github.com/spicyjack/jenkins-cfg.git
+# Issues:   https://github.com/spicyjack/jenkins-cfg/issues
+
+### MAIN SCRIPT ###
+# what's my name?
+SCRIPTNAME=$(basename $0)
+# path to the perl binary
+
+# verbose script output by default
+QUIET=0
+
+# default exit status
+EXIT_STATUS=0
+
+### SCRIPT SETUP ###
+# source jenkins functions
+. ~/src/jenkins-cfg.git/scripts/common_jenkins_functions.sh
+
+#check_env_variable "$PRIVATE_STAMP_DIR" "PRIVATE_STAMP_DIR"
+#check_env_variable "$PUBLIC_STAMP_DIR" "PUBLIC_STAMP_DIR"
+
+GETOPT_SHORT="hqp:"
+GETOPT_LONG="help,quiet,path:"
+# sets GETOPT_TEMP
+# pass in $@ unquoted so it expands, and run_getopt() will then quote it "$@"
+# when it goes to re-parse script arguments
+run_getopt "$GETOPT_SHORT" "$GETOPT_LONG" $@
+
+show_help () {
+cat <<-EOF
+
+    ${SCRIPTNAME} [options]
+
+    SCRIPT OPTIONS
+    -h|--help       Displays this help message
+    -q|--quiet      No script output (unless an error occurs)
+    -p|--path       Path to delete (usually \$WORKSPACE/output)
+
+    Example usage:
+    ${SCRIPTNAME} --path \$WORKSPACE/output
+EOF
+}
+
+# Note the quotes around `$TEMP': they are essential!
+# read in the $TEMP variable
+eval set -- "$GETOPT_TEMP"
+
+# read in command line options and set appropriate environment variables
+while true ; do
+    case "$1" in
+        -h|--help) # show the script options
+            show_help
+            exit 0;;
+        -q|--quiet)    # don't output anything (unless there's an error)
+            QUIET=1
+            shift;;
+        -p|--path) # dependencies to check for
+            DELETE_DIR="$2";
+            shift 2;;
+        --) shift;
+            break;;
+        *) # we shouldn't get here; die gracefully
+            echo "ERROR: unknown option '$1'" >&2
+            echo "ERROR: use --help to see all script options" >&2
+            exit 1
+            ;;
+    esac
+done
+
+if [ "x$DELETE_DIR" = "x" ]; then
+    echo "ERROR: Please pass a path to the jenkins-cfg.git directory (--path)"
+    exit 1
+fi
+
+if [ ! -d "$DELETE_DIR" ]; then
+    echo "ERROR: jenkins-cfg.git path ${DELETE_DIR} does not exist"
+    exit 1
+fi
+
+### SCRIPT MAIN LOOP ###
+show_script_header
+if [ $QUIET -ne 1 ]; then
+    if [ -d ${DELETE_DIR} ]; then
+        echo "-> Running 'rm -rf' on ${DELETE_DIR}"
+        rm -rf $DELETE_DIR
+        EXIT_STATUS=$?
+    else
+        echo "WARNING: ${DELETE_DIR} does not exist"
+    fi
+fi
+
+if [ $EXIT_STATUS -gt 0 ]; then
+    echo "ERROR: Something happened while deleting ${DELETE_DIR}"
+fi
+
+exit $EXIT_STATUS
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; version 2 dated June, 1991.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program;  if not, write to the Free Software
+#   Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+# vi: set filetype=sh shiftwidth=4 tabstop=4
+# end of line

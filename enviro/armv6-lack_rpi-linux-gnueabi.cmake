44188914c6666acc8922412fe7261e12353a6dae
Brian Manning <brian@xaoc.org>
2013-09-16 15:36:40 -0700

    armv6-lack_rpi-linux-gnueabi.cmake: added CMake setup file

---
+++ enviro/armv6-lack_rpi-linux-gnueabi.cmake
@@ -0,0 +1,18 @@
+# this one is important
+SET(CMAKE_SYSTEM_NAME Linux)
+include(CMakeForceCompiler)
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER /opt/cross/armv6-lack_rpi-linux-gnueabi/bin/armv6-lack_rpi-linux-gnueabi-gcc)
+SET(CMAKE_CXX_COMPILER /opt/cross/armv6-lack_rpi-linux-gnueabi/bin/armv6-lack_rpi-linux-gnueabi-g++)
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH /opt/cross/armv6-lack_rpi-linux-gnueabi/armv6-lack_rpi-linux-gnueabi/sysroot )
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)

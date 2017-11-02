0ac759a669d91f70920f4adff75ac47c18365bef
Brian Manning <brian@xaoc.org>
2017-11-02 11:46:19 -0700

    common_jenkins_functions.sh: converted to 3-space "tabs"

--- scripts/common_jenkins_functions.sh
+++ scripts/common_jenkins_functions.sh
@@ -6,7 +6,7 @@
 # License terms are listed at the bottom of this file
 #
 # Impotant URLs:
-# Clone:    https://github.com/spicyjack/jenkins-config.git
+# Clone:   https://github.com/spicyjack/jenkins-config.git
 # Issues:   https://github.com/spicyjack/jenkins-config/issues
 
 ### Jenkins text display shell functions ###
@@ -16,10 +16,10 @@
 ## ENV:  QUIET - the quietness level of the script
 ## DESC: Check if $QUIET is set, and if not, write MESSAGE to STDOUT
 function say {
-    local MESSAGE="$1"
-    if [ $QUIET -ne 1 ]; then
-        echo "$MESSAGE"
-    fi
+   local MESSAGE="$1"
+   if [ $QUIET -ne 1 ]; then
+      echo "$MESSAGE"
+   fi
 }
 
 ## FUNC: info()
@@ -27,18 +27,18 @@ function say {
 ## ENV:  QUIET - the quietness level of the script
 ## DESC: Check if $QUIET is set, and if not, write MESSAGE to STDOUT
 function info {
-    local MESSAGE="$1"
-    if [ $QUIET -ne 1 ]; then
-        echo "-> $MESSAGE"
-    fi
+   local MESSAGE="$1"
+   if [ $QUIET -ne 1 ]; then
+      echo "-> $MESSAGE"
+   fi
 }
 
 ## FUNC: warn()
 ## ARG:  MESSAGE - the message to be written to STDERR
 ## DESC: Always writes MESSAGE to STDERR (ignores $QUIET)
 function warn {
-    local MESSAGE="$1"
-    echo $MESSAGE >&2
+   local MESSAGE="$1"
+   echo $MESSAGE >&2
 }
 
 ### Other Jenkins shell functions ###
@@ -48,26 +48,26 @@ function warn {
 ## DESC: Adds any additional paths specified in front of all other paths in the
 ## DESC: user's $PATH environment variable
 add_additional_paths () {
-    local ADDITIONAL_PATHS="$1"
+   local ADDITIONAL_PATHS="$1"
 
-    for ADDPATH in $(echo ${ADDITIONAL_PATHS});
-    do
-        if [ $(echo ${PATH} | grep -c ${ADDPATH}) -eq 0 ]; then
-            PATH=${ADDPATH}:$PATH
-        #else
-        #    echo "Path: $ADDPATH already exists in \$PATH"
-        fi
-    done
-    unset ADDITONAL_PATHS ADDPATH
-    # XXX to export or not to export, that is the question
-    export PATH
+   for ADDPATH in $(echo ${ADDITIONAL_PATHS});
+   do
+      if [ $(echo ${PATH} | grep -c ${ADDPATH}) -eq 0 ]; then
+         PATH=${ADDPATH}:$PATH
+      #else
+      #   echo "Path: $ADDPATH already exists in \$PATH"
+      fi
+   done
+   unset ADDITONAL_PATHS ADDPATH
+   # XXX to export or not to export, that is the question
+   export PATH
 }
 
 ## FUNC: add_usr_local_paths()
 ## DESC: Adds paths in /usr/local (/usr/local/bin, /usr/local/sbin) via the
 ## DESC: add_additonal_paths shell function
 add_usr_local_paths () {
-    add_additional_paths "/usr/local/bin /usr/local/sbin"
+   add_additional_paths "/usr/local/bin /usr/local/sbin"
 }
 
 ## FUNC: check_env_variable()
@@ -75,13 +75,13 @@ add_usr_local_paths () {
 ## ARG:  ENV_VAR, the environment variable to check
 ## DESC: Checks to see if a variable is set in the environment
 check_env_variable () {
-    local ENV_VAR_NAME="$1"
-    local ENV_VAR="$2"
+   local ENV_VAR_NAME="$1"
+   local ENV_VAR="$2"
 
-    if [ -z $ENV_VAR ]; then
-        warn "ERROR: environment variable ${ENV_VAR_NAME} unset"
-        exit 1
-    fi
+   if [ -z $ENV_VAR ]; then
+      warn "ERROR: environment variable ${ENV_VAR_NAME} unset"
+      exit 1
+   fi
 }
 
 ## FUNC: check_exit_status()
@@ -91,22 +91,22 @@ check_env_variable () {
 ## DESC: Verifies the function exited with an exit status code (0), and
 ## DESC: exits the script if any other status code is found.
 check_exit_status () {
-    EXIT_STATUS="$1"
-    DESC="$2"
-    OUTPUT="$3"
+   EXIT_STATUS="$1"
+   DESC="$2"
+   OUTPUT="$3"
 
-    if [ $QUIET -ne 1 ]; then
-        # check for errors from the script
-        if [ $EXIT_STATUS -ne 0 ] ; then
-            warn "${SCRIPTNAME}: ${DESC}"
-            warn "${SCRIPTNAME}: error exit status: ${EXIT_STATUS}"
-        fi
-        if [ "x$OUTPUT" != "x" ]; then
-            echo "${SCRIPTNAME}: ${DESC} output:"
-            echo $OUTPUT
-       fi
-    fi
-    return $EXIT_STATUS
+   if [ $QUIET -ne 1 ]; then
+      # check for errors from the script
+      if [ $EXIT_STATUS -ne 0 ] ; then
+         warn "${SCRIPTNAME}: ${DESC}"
+         warn "${SCRIPTNAME}: error exit status: ${EXIT_STATUS}"
+      fi
+      if [ "x$OUTPUT" != "x" ]; then
+         echo "${SCRIPTNAME}: ${DESC} output:"
+         echo $OUTPUT
+      fi
+   fi
+   return $EXIT_STATUS
 } # check_exit_status
 
 ## FUNC: generate_artifact_timestamp()
@@ -116,7 +116,7 @@ check_exit_status () {
 ## DESC: time, you can tell when it was built, and with what version of the
 ## DESC: source code it was built
 function generate_artifact_timestamp {
-    ARTIFACT_TIMESTAMP=$(date +%Y.%j-%H.%m)
+   ARTIFACT_TIMESTAMP=$(date +%Y.%j-%H.%m)
 }
 
 ## FUNC: run_getopt status()
@@ -127,56 +127,56 @@ function generate_artifact_timestamp {
 ## DESC: Sets up command line arguments for processing in the main script;
 ## DESC: Detects which OS and what versions of 'getopt' are available
 run_getopt() {
-    # use 'shift' to remove the first two arguments, prior to running getopt
-    # with the value of "$@"
-    local GETOPT_SHORT="$1"
-    shift
-    local GETOPT_LONG="$1"
-    shift
+   # use 'shift' to remove the first two arguments, prior to running getopt
+   # with the value of "$@"
+   local GETOPT_SHORT="$1"
+   shift
+   local GETOPT_LONG="$1"
+   shift
 
-    # these two paths cover a majority of my test machines
-    for GETOPT_CHECK in "/opt/local/bin/getopt" "/usr/bin/getopt";
-    do
-        if [ -x "${GETOPT_CHECK}" ]; then
-            GETOPT_BIN=$GETOPT_CHECK
-            break
-        fi
-    done
+   # these two paths cover a majority of my test machines
+   for GETOPT_CHECK in "/opt/local/bin/getopt" "/usr/bin/getopt";
+   do
+      if [ -x "${GETOPT_CHECK}" ]; then
+         GETOPT_BIN=$GETOPT_CHECK
+         break
+      fi
+   done
 
-    # did we find an actual binary out of the list above?
-    if [ -z "${GETOPT_BIN}" ]; then
-        warn "ERROR: getopt binary not found; exiting...."
-        exit 1
-    fi
+   # did we find an actual binary out of the list above?
+   if [ -z "${GETOPT_BIN}" ]; then
+      warn "ERROR: getopt binary not found; exiting...."
+      exit 1
+   fi
 
-    OS_NAME=$(/usr/bin/env uname -s)
-    if [ $OS_NAME = "Darwin" -a $GETOPT_BIN != "/opt/local/bin/getopt" ]; then
-        # Use short options if we're using Darwin's getopt
-        GETOPT_TEMP=$(${GETOPT_BIN} ${GETOPT_SHORT} $*)
-    else
-        # Use short and long options with GNU's getopt
-        GETOPT_TEMP=$(${GETOPT_BIN} -o ${GETOPT_SHORT} \
-            --long ${GETOPT_LONG} \
-            -n "${SCRIPTNAME}" -- "$@")
-    fi
+   OS_NAME=$(/usr/bin/env uname -s)
+   if [ $OS_NAME = "Darwin" -a $GETOPT_BIN != "/opt/local/bin/getopt" ]; then
+      # Use short options if we're using Darwin's getopt
+      GETOPT_TEMP=$(${GETOPT_BIN} ${GETOPT_SHORT} $*)
+   else
+      # Use short and long options with GNU's getopt
+      GETOPT_TEMP=$(${GETOPT_BIN} -o ${GETOPT_SHORT} \
+         --long ${GETOPT_LONG} \
+         -n "${SCRIPTNAME}" -- "$@")
+   fi
 
-    # if getopts exited with an error code, then exit the script
-    #if [ $? -ne 0 -o $# -eq 0 ] ; then
-    if [ $? != 0 ] ; then
-        warn "Run '${SCRIPTNAME} --help' to see script options"
-        exit 1
-    fi
+   # if getopts exited with an error code, then exit the script
+   #if [ $? -ne 0 -o $# -eq 0 ] ; then
+   if [ $? != 0 ] ; then
+      warn "Run '${SCRIPTNAME} --help' to see script options"
+      exit 1
+   fi
 }
 
 ## FUNC: show_script_header()
 ## ENV:  QUIET - the quietness level of the script
 ## DESC: Prints out a nicely formatted script header, if $QUIET is not set
 show_script_header () {
-    if [ $QUIET -ne 1 ]; then
-        RUN_DATE=$(date +"%a %b %d %T %Z %Y (%Y.%j)")
-        say "=-=-= ${SCRIPTNAME} =-=-="
-        info "Run date: ${RUN_DATE}"
-    fi
+   if [ $QUIET -ne 1 ]; then
+      RUN_DATE=$(date +"%a %b %d %T %Z %Y (%Y.%j)")
+      say "=-=-= ${SCRIPTNAME} =-=-="
+      info "Run date: ${RUN_DATE}"
+   fi
 }
 
 #   This program is free software; you can redistribute it and/or modify
